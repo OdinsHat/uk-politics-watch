4e98fa702920774f5bc47c4cc0e2d6c20093abb0
Doug Bromley <doug.bromley@gmail.com>
2014-06-09 22:53:13 +0100

    Adding placeholder event hanlers for omnibox keyword

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -6,4 +6,21 @@ chrome.runtime.onInstalled.addListener(function (details) {
 
 chrome.browserAction.setBadgeText({text: '\'Allo'});
 
+chrome.omnibox.onInputChanged.addListener(
+    function(text, suggest) {
+        console.log('inputChanged (search hansard): ' + text);
+        suggest([
+            {content: text + ' one', description: "the first one"},
+            {content: text + ' number two', description: "the second entry"}
+        ]);
+    }
+);
+
+chrome.omnibox.onInputEntered.addListener(
+    function(text) {
+        console.log('inputEntered: ' + text);
+        alert('You just typed "' + text + '"');
+    }
+);
+
 console.log('\'Allo \'Allo! Event Page for Browser Action');
