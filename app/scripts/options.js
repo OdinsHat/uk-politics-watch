8fa2116a06be5b9ade09fb6ee4cf671b088581af
Doug Bromley <doug.bromley@gmail.com>
2014-06-14 22:15:40 +0100

    Moved the JS from the old extensions options.html script tags into its own file
    Much work to be done as usual - I dont even understand how this ever worked

--- app/scripts/options.js
+++ app/scripts/options.js
@@ -1,3 +1,27 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Option');
+var postcodeTextbox;
+var saveButton;
+
+init();
+
+function init() {
+  postcodeTextbox = document.getElementById("postcode");
+  saveButton = document.getElementById("save-button");
+
+  postcodeTextbox.value = localStorage.postcode || "";
+  markClean();
+}
+
+function save() {
+  localStorage.postcode = postcodeTextbox.value;
+  markClean();
+}
+
+function markDirty() {
+  saveButton.disabled = false;
+}
+
+function markClean() {
+  saveButton.disabled = true;
+}
